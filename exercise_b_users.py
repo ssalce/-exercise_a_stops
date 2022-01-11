ae4279ef3aa61a5681d89e7b4017568a8fda81bf
Sofia <sofiasdowns@gmail.com>
2022-01-11 23:24:49 +0000

    intent exercise_b_users homework

--- exercise_b_users.py
+++ exercise_b_users.py
@@ -38,6 +38,10 @@ users = {
     {
       "name": "rupert",
       "species": "parrot"
+    },
+    {
+      "name": "fluffy"
+      "species": "dog"
     }
   ]
   },
@@ -55,12 +59,22 @@ users = {
 }
 
 # 1. Get Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
+print(users ["Jonathan"]["twitter"])
 # 2. Get Erik's hometown
+print(users["Erik"]["home_town"])
 # 3. Get the list of Erik's lottery numbers
+print(users ["Erik"]["lottery_numbers"])
 # 4. Get the species of Avril's pet Monty
+
 # 5. Get the smallest of Erik's lottery numbers
+
 # 6. Return an list of Avril's lottery numbers that are even
+
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
+
 # 8. Change Erik's hometown to Edinburgh
+users["Erik"]["hometown"]= "Edinburgh"
+print(users["Erik"]["hometown"])
 # 9. Add a pet dog to Erik called "fluffy"
+print(users["Erik"]["pets"])
 # 10. Add another person to the users dictionary
